04844852e91f3ef79f5b40a5e46daed9e65d7598
Sidney Pang <sidneypang@hotmail.co.uk>
2023-05-08 17:44:41 -0400

    testing coliders

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
-import { Physics, RigidBody, Debug } from "@react-three/rapier";
+import { Physics, RigidBody, Debug, CuboidCollider, BallCollider } from "@react-three/rapier";
 
 export default function Experience() {
   return (
@@ -16,14 +16,14 @@ export default function Experience() {
       <Physics>
         <Debug />
 
-        <RigidBody>
-          <mesh castShadow position={[-2, 2, 0]}>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[0, 4, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        <RigidBody>
+        {/* <RigidBody>
           <mesh castShadow position={[2, 2, 0]}>
             <boxGeometry args={[3, 2, 1]} />
             <meshStandardMaterial color="mediumpurple" />
@@ -31,6 +31,18 @@ export default function Experience() {
 
           <mesh castShadow position={[2, 2, 3]}>
             <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody> */}
+
+        <RigidBody
+          colliders={false}
+          position={[0, 1, 0]}
+          rotation={[Math.PI * 0.5, 0, 0]}
+        >
+          <BallCollider args={[1.5]} />
+          <mesh castShadow>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
         </RigidBody>
